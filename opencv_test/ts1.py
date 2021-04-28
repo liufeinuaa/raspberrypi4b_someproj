b2de15b3fde1c225524dde2c51745d962a119bbd
liufei <55144027+liufeinuaa@users.noreply.github.com>
2021-04-28 22:55:59 +0800

    some change

---
+++ opencv_test/ts1.py
@@ -0,0 +1,19 @@
+'''
+from opencv.org tutorials
+https://docs.opencv.org/4.5.2/d9/d61/tutorial_py_morphological_ops.html
+
+Image gradients
+
+'''
+
+import cv2 as cv
+import numpy as np
+
+
+
+
+
+
+
+
+
